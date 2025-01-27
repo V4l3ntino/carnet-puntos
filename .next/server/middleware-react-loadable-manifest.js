f8015b2d038dcc592e53deeebdfe23e8003f17ab
V4l3ntino <valentinohontar15@gmail.com>
2025-01-27 20:40:28 +0100

    Eliminando .next del repositorio

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"node_modules/@mui/x-data-grid-premium/esm/hooks/features/export/serializer/excelSerializer.js -> exceljs\":{\"id\":\"node_modules/@mui/x-data-grid-premium/esm/hooks/features/export/serializer/excelSerializer.js -> exceljs\",\"files\":[]}}"
